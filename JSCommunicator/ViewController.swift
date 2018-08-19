246c2717c5f519cda4ee0722458ecd9059daa7db
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-08-19 10:52:02 +0900

    Initial Commit

---
+++ JSCommunicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSCommunicator
+//
+//  Created by mw on 2018/08/19.
+//  Copyright © 2018年 mw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
